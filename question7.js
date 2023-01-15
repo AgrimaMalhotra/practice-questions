a6ce464a4cb5ed6b3be93633c3fee83ec2e27754
Agrima <er.agrima.malhotra@gmail.com>
2023-01-15 21:10:22 +0530

    feat: add concatinating strings

---
+++ question7.js
@@ -0,0 +1,17 @@
+//Normal function implementation
+function concatStrings(input){
+    return input.join('').slice(1);
+}
+
+//Arrow function implementation
+let _concatStrings = input =>{
+    return input.join('').slice(1);
+}
+
+//Sample Case using Normal function implementation
+stringInput=["code", "academy"];
+console.log(concatStrings(stringInput));
+
+//Sample Case using Arrow function implementation
+stringInput=["code", "academy"];
+console.log(_concatStrings(stringInput));
