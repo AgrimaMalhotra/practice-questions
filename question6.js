6f6b619c4da0ec73d511615fed958edc21fb02b7
Agrima <er.agrima.malhotra@gmail.com>
2023-01-15 21:06:06 +0530

    feat: add check string ending with 'Script'

---
+++ question6.js
@@ -0,0 +1,31 @@
+//Normal function implementation
+
+//helper function to check if ends with 'Script'
+function check(strInput){
+    return strInput.endsWith('Script') ? true : false;}
+
+// main function to perform the check
+function checkEndsWith(input){
+    //ternary condition to check for length of input to be greater than equal 6
+    return input.length >= 6 ? check(input) : false ;
+}
+
+//Arrow function implementation
+
+//helper function to check if ends with 'Script'
+let _check = strInput =>{
+    return strInput.endsWith('Script') ? true : false;}
+
+// main function to perform the check
+let _checkEndsWith = input => {
+    //ternary condition to check for length of input to be greater than equal 6
+    return input.length >= 6 ? _check(input) : false ;
+}
+
+//Sample Input for Normal function implementation
+stringInput='javaScript';
+console.log(checkEndsWith(stringInput));
+
+//Sample Input for Arrow function implementation
+stringInput='javaScript';
+console.log(_checkEndsWith(stringInput));
