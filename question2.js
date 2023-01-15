6ab7fff91964fb56adf3e54e89a99a7b00f5246f
Agrima <er.agrima.malhotra@gmail.com>
2023-01-16 02:14:08 +0530

    feat: add check for same digits in a number

--- question2.js
+++ question2.js
@@ -2,14 +2,17 @@
 function sameDigits(input){
     //extract digit at units place
     const unitDigit = input % 10;
+    input = parseInt(input / 10);
     //flag used to indicate in case of different digits
     let flag=false;
     while(input>0){
         let currentDigit = input % 10;
         //check if any digit different from unit digit
-        unitDigit === currentDigit? flag = true : flag = false;
+        flag = unitDigit === currentDigit? true : false;
         input = parseInt(input / 10);
-
+        if (!flag) {
+            break;
+        }
     }
     //return value based on flag set
     return flag? true : false;
@@ -19,14 +22,17 @@ function sameDigits(input){
 let _sameDigits = input => {
     //extract digit at units place
     const unitDigit = input % 10;
+    input = parseInt(input / 10);
     //flag used to indicate in case of different digits
     let flag=false;
     while(input>0){
         let currentDigit = input % 10;
         //check if any digit different from unit digit
-        unitDigit === currentDigit? flag = true : flag = false;
+        flag = unitDigit === currentDigit? true : false;
         input = parseInt(input / 10);
-
+        if (!flag) {
+            break;
+        }
     }
     //return value based on flag set
     return flag? true : false;
@@ -39,3 +45,6 @@ console.log(sameDigits(number));
 //Sample Case - 2 using arrow function declaration 
 number= 23;
 console.log(_sameDigits(number));
+
+//jest testing
+module.exports = {sameDigits, _sameDigits};
