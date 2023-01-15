51eb32b58f4834a4853e33b141c0e8c215710ceb
Agrima <er.agrima.malhotra@gmail.com>
2023-01-15 21:53:11 +0530

    feat: add check leap year

--- question5.js
+++ question5.js
@@ -1,17 +1,15 @@
 //Normal function implementation
 function checkLeapYear(input){
-    return input%4 === 0 ? true : false;
+    return input%4 === 0 && input%100!=0 || input%400 == 0 ? true : false;
 }
 
 //Arrow function implementation
-let _checkLeapYear =input => {
-    return input%4 === 0 ? true : false;
-}
+let _checkLeapYear = input => input%4 === 0 && input%100!=0 || input%400 == 0 ? true : false;
 
 //Sample Input with normal function implementation
 yearInput=2000;
 console.log(checkLeapYear(yearInput));
 
 //Sample Input with arrow function implementation
-yearInput=2000;
+yearInput=100;
 console.log(_checkLeapYear(yearInput));
