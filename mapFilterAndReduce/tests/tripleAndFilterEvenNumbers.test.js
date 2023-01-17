b49b02a88b9a1f1f1cfa4e9f1fb893ccae9a65b1
Agrima <er.agrima.malhotra@gmail.com>
2023-01-17 09:45:04 +0530

    test: triple even array elements

--- mapFilterAndReduce/tests/tripleAndFilterEvenNumbers.test.js
+++ mapFilterAndReduce/tests/tripleAndFilterEvenNumbers.test.js
@@ -1,4 +1,4 @@
-const { getTripledEvenArray } = require('practice-questions/tripleAndFilterEvenNumbers.js');
+const { getTripledEvenArray, _getTripledEvenArray } = require('practice-questions/mapFilterAndReduce/problem/tripleAndFilterEvenNumbers.js');
 
 describe('Array Utility', () => {
   describe('Triple every array element and filter out even numbers', () => {
@@ -11,5 +11,16 @@ describe('Array Utility', () => {
     it('should throw Type Error if all array elements not int', () => {
       expect(() => getTripledEvenArray([1, 2, 3, 4, 'str', 6])).toThrow('Type Error');
     });
-  })
-})
+  });
+  describe('Triple every array element and filter out even numbers using reduce', () => {
+    it('should return an array of tripled even numbers', () => {
+      expect(_getTripledEvenArray([1, 2, 3, 4, 5, 6])).toEqual([6, 12, 18]);
+    });
+    it('should throw Type Error if input not array', () => {
+      expect(() => _getTripledEvenArray('str')).toThrow('Type Error');
+    });
+    it('should throw Type Error if all array elements not int', () => {
+      expect(() => _getTripledEvenArray([1, 2, 3, 4, 'str', 6])).toThrow('Type Error');
+    });
+  });
+});
